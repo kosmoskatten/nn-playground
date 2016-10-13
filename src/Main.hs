1d97a6196d89fa0e64ede508f986c73e30c96cc9
Patrik Sandahl <patrik.sandahl@ericsson.com>
2016-10-13 13:15:40 +0200

    Indentation.

--- src/Main.hs
+++ src/Main.hs
@@ -7,12 +7,12 @@ import Network (mkLiteralNetwork, feedForward)
 
 main :: IO ()
 main = do
-  let hidden  = mkLiteralLayer [ [0.15, 0.2]
-                               , [0.25, 0.3]
-                               ] [0.35, 0.35]
-      output  = mkLiteralLayer [ [ 0.4, 0.45]
-                               , [ 0.5, 0.55]
-                               ] [0.6, 0.6]
-      network = mkLiteralNetwork hidden output
+    let hidden  = mkLiteralLayer [ [0.15, 0.2]
+                                 , [0.25, 0.3]
+                                 ] [0.35, 0.35]
+        output  = mkLiteralLayer [ [ 0.4, 0.45]
+                                 , [ 0.5, 0.55]
+                                 ] [0.6, 0.6]
+        network = mkLiteralNetwork hidden output
 
-  print $ feedForward network $ fromList [ 0.05, 0.1 ]
+    print $ feedForward network $ fromList [ 0.05, 0.1 ]
